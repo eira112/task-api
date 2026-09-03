c8c0267075c499133ceb6fa8d0db05fdaa7cd9d6
eira112 <anushkabhaju@gmail.com>
2026-09-03 11:24:51 +0545

    fix task API tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -4,65 +4,132 @@ from database import SessionLocal
 from models.db_models import Task
 import pytest
 
-client=TestClient(app)
+client = TestClient(app)
+
 
 @pytest.fixture
 def clean_tasks():
-    db=SessionLocal()
+    db = SessionLocal()
+
     def cleanup(task_id):
-        task=db.get(Task,task_id)
+        task = db.get(Task, task_id)
+
         if task:
             db.delete(task)
             db.commit()
+
     yield cleanup
     db.close()
 
 
 def test_home():
-    response=client.get("/")
-    assert response.status_code==200
-    assert response.json()=={"message":"task api is running"}
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Task API is running"
+    }
+
 
 def test_create_task(clean_tasks):
-    response=client.post("/tasks", json={"title":"add authentication"})
-    task_id=response.json()["task"]["id"]
-    assert response.status_code==201
-    assert response.json()=={"message":"task created","task":{"id":task_id,"title":"add authentication","completed":False}}
+    response = client.post(
+        "/tasks",
+        json={"title": "add authentication"}
+    )
+
+    task_id = response.json()["task"]["id"]
+
+    assert response.status_code == 201
+    assert response.json() == {
+        "message": "task created",
+        "task": {
+            "id": task_id,
+            "title": "add authentication",
+            "completed": False
+        }
+    }
+
     clean_tasks(task_id)
+
 
 def test_get_tasks(clean_tasks):
-    create_response=client.post("/tasks",json={"title":"test task"})
-    task_id=create_response.json()["task"]["id"]
-    response=client.get("/tasks")
-    tasks=response.json()
-    current_task=None
+    create_response = client.post(
+        "/tasks",
+        json={"title": "test task"}
+    )
+
+    task_id = create_response.json()["task"]["id"]
+
+    response = client.get("/tasks")
+
+    tasks = response.json()
+
+    current_task = None
+
     for task in tasks:
-        if(task["id"]==task_id):
-            current_task=task
-    assert response.status_code==200
-    assert current_task=={"id":task_id,"title":"test task","completed":False}
+        if task["id"] == task_id:
+            current_task = task
+            break
+
+    assert response.status_code == 200
+    assert current_task == {
+        "id": task_id,
+        "title": "test task",
+        "completed": False
+    }
+
     clean_tasks(task_id)
 
-def test_get_task(clean_tasks):
-    create_response=client.post("/tasks",json={"title":"test task"})
-    task_id=create_response.json()["task"]["id"]
-    response=client.get(f"/tasks/{task_id}")
-    assert response.status_code==200
-    assert response.json()=={"id":task_id,"title":"test task","completed":False}
+
+def test_get_single_task(clean_tasks):
+    create_response = client.post(
+        "/tasks",
+        json={"title": "single task"}
+    )
+
+    task_id = create_response.json()["task"]["id"]
+
+    response = client.get(f"/tasks/{task_id}")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": task_id,
+        "title": "single task",
+        "completed": False
+    }
+
     clean_tasks(task_id)
 
-def test_complete_task(clean_tasks):
-    create_response=client.post("/tasks",json={"title":"test task"})
-    task_id=create_response.json()["task"]["id"]
-    response= client.put(f"/tasks/{task_id}")
-    assert response.status_code==200
-    assert response.json()=={"id":task_id,"title":"test task","completed":True}
+
+def test_update_task(clean_tasks):
+    create_response = client.post(
+        "/tasks",
+        json={"title": "update task"}
+    )
+
+    task_id = create_response.json()["task"]["id"]
+
+    response = client.put(f"/tasks/{task_id}")
+
+    assert response.status_code == 200
+    assert response.json()["completed"] is True
+
     clean_tasks(task_id)
+
 
 def test_delete_task(clean_tasks):
-    create_response=client.post("/tasks",json={"title":"test task"})
-    task_id=create_response.json()["task"]["id"]
-    response= client.delete(f"/tasks/{task_id}")
-    assert response.status_code==200
-    assert response.json()=={"message":"task removed from the list"}
+    create_response = client.post(
+        "/tasks",
+        json={"title": "delete task"}
+    )
 
+    task_id = create_response.json()["task"]["id"]
+
+    response = client.delete(f"/tasks/{task_id}")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "task removed from the list"
+    }
+
+    clean_tasks(task_id)
